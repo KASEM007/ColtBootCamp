682f5777d63f3b412c0c683b23b01e8e1bbd8e31
Kasem007 <kasem4coding@gmail.com>
2021-02-23 05:58:43 -0500

    Item Lister

--- demo/dom/dist/dom.dev.js
+++ demo/dom/dist/dom.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Examine the document Object//
 // console.log(document.domain);
 // console.log(document.URL);
@@ -6,22 +8,11 @@
 // console.log(document.body);
 // console.log(document.all[10]);
 // document.all[10]
-
 // console.log(document.forms);
 // console.log(document.links);
 // const headerTitle = document.getElementById('header-title');
 // headerTitle.textContent = "hiiiii!!!";
 // headerTitle.innerText = "Goodbye!!";
 // headerTitle.innerHTML = '<h3> Hello from h3 </h3>'
-const header = document.getElementById('main-header');
+var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px grey';
-
-
-
-
-
-
-
-
-
-
