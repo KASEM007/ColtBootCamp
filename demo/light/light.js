41fcc74e50cd72048cdefac79734f13cbe3ab174
Kasem007 <kasem4coding@gmail.com>
2021-03-07 06:05:23 -0500

    factorial in js

--- demo/light/light.js
+++ demo/light/light.js
@@ -36,17 +36,60 @@
 // *******************************************
 // Program to count down numbers to 1.
 
-function countDown(number){
-   // number = parseFloat(prompt("Enter a number: "))
-   //display the number
-    console.log(number);
-    //decrease the number value
-    const newNumber = number - 1;
-    // base case
-    if(newNumber > 0){
-        countDown(newNumber)   
+// function countDown(number){
+//    // number = parseFloat(prompt("Enter a number: "))
+//    //display the number
+//     console.log(number);
+//     //decrease the number value
+//     const newNumber = number - 1;
+//     // base case
+//     if(newNumber > 0){
+//         countDown(newNumber)   
+//     }
+// }
+
+// countDown(5);
+
+// *******************************************
+// *******************************************
+
+// let i = 2;
+// for(let j=0; i <= 11; j++){
+//     i *= j;
+//     console.log(++i);
+// }
+
+// *******************************************
+// *******************************************
+
+// program to find the factorial of a number
+function factorial(x){
+    //if number is 0
+    if(x == 0){
+        return 1;
+    }
+    //if number is positive
+    else{
+        console.log(x);
+        return x * factorial(x - 1);
     }
 }
 
-countDown(5);
+const num = 3;
+// calling factorial() if num is non-negative
+if(num > 0){
+    let result = factorial(num);
+    console.log(`The factorial of ${num} is ${result}`);
+
+}
+
+
+
+
+
+
+
+
+
+
 
